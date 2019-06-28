d5b6b15de3199be97d7560e6cd753122d0e1058f
Shelby McCowan <smccowan33@gmail.com>
2019-06-28 13:20:18 -0600

    Added Source model.

---
+++ models/sourcemodel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const SourceSchema = mongoose.Schema({
+  name: {
+    type: String
+  },
+  followers: {
+    type: Number
+  },
+  questions: {
+    type: Number
+  },
+  posterID: {
+    type: String
+  },
+  views: {
+    type: String
+  },
+  subjectURL:{
+    type: String
+  }
+});
+
+const Source = module.exports = mongoose.model('Source', SourceSchema);
