871359b94a13c3fe2754dd332d736fe8adb127b6
Shelby McCowan <smccowan33@gmail.com>
2019-06-28 10:51:10 -0600

    Added topics routes

---
+++ routes/topics.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/usermodel')
+const Question = require('../models/questionmodel')
+const Answer = require('../models/answermodel')
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const AutoRes = require('../RouteUtils/autores')
+const config = require('../config/database')
+
+
+
+module.exports = router;
