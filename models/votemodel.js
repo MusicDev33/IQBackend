2fb549eccb1ac8704bc405d68b56c0946cc8a3cd
Shelby McCowan <smccowan33@gmail.com>
2019-07-10 13:43:59 -0600

    Vote model is ready

--- models/votemodel.js
+++ models/votemodel.js
@@ -14,16 +14,40 @@ const VoteSchema = mongoose.Schema({
 
 const Vote = module.exports = mongoose.model('Vote', VoteSchema);
 
-// Send callback as err, savedObject, oldObject?
+// Send callback as err, savedObject, oldObject...msg maybe?
+// TODO: Clean this shit up, just wow.
+// This is awful. Someone needs to fire me.
 module.exports.addVote = function(userID, answerID, voteInt, callback){
   Vote.findOne({userid: userID, answerid: answerID}, (err, vote) => {
     if (err) throw err;
     if (vote){
       if (vote.vote === voteInt){
-        callback(null, null)
+        callback(null, null, null)
       }else{
-
+        Vote.findOneAndUpdate({userid: userID, answerid: answerID},
+          {vote: voteInt}, (err, updatedVote) => {
+            if (err) throw err;
+            if (updatedVote){
+              callback(null, vote, updatedVote)
+            }else{
+              callback(null, null, null)
+            }
+          })
       }
+    }else{
+      newVote = new Vote({
+        userid: userID,
+        answerid: answerID,
+        vote: voteInt
+      })
+      newVote.save( (err, savedVote) => {
+        if (err) throw err;
+        if (savedVote){
+          callback(null, savedVote)
+        }else{
+          callback(null, null)
+        }
+      })
     }
   })
 }
